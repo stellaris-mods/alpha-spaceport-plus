36edba65d9ab93a7070cb50f6bda1d0c9e3f3b02
Folk <vhaarr@gmail.com>
2017-10-15 17:36:33 +0200

    Initial upload.

---
+++ modinfo.lua
@@ -0,0 +1,15 @@
+-- modinfo read by stlrel from http://github.com/stellaris-mods/scripts
+return {
+	path = "AlphaModPlus",
+	name = "AlphaModPlus (1.8)",
+	tags = { "Gameplay", "Technologies", "Spaceports", "AlphaMod" },
+	picture = "thumb.jpg",
+	supported_version = "1.8.*",
+	remote_file_id = 736283200,
+	--readme = "README.md",
+	--steambb = "steam.bbcode",
+	--bbheadinglevel = 3,
+	-- Files and folders added in exclude will not be copied over to the Paradox Interactive
+	-- local mod folder when you run `stlrel --steam`.
+	exclude = { ".git" }
+}
